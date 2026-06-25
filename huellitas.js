216d5222b22ae1a4e7776d6c1d79817ed355e589
carloslira0223-cyber <carloslira0223@gmail.com>
2026-06-24 21:51:09 -0600

    Load final UX guard modules

--- huellitas.js
+++ huellitas.js
@@ -1,5 +1,5 @@
 (function () {
-    const version = "20260624-final-polish";
+    const version = "20260624-final-ux";
 
     function writeScript(src) {
         document.write('<script src="' + src + '?v=' + version + '"><\/script>');
@@ -16,21 +16,27 @@
         writeScript("huellitas-original.js");
         writeScript("huellitas-fixes.js");
         writeScript("huellitas-pet-polish.js");
+        writeScript("huellitas-profile-touch.js");
         writeScript("huellitas-profile-mobile.js");
         writeScript("huellitas-pet-color.js");
         writeScript("huellitas-lost-pets.js");
         writeScript("huellitas-admin-secure.js");
         writeScript("huellitas-admin-pro.js");
         writeScript("huellitas-final-polish.js");
+        writeScript("huellitas-ux-guard.js");
     } else {
         appendScript("huellitas-original.js", () => {
             appendScript("huellitas-fixes.js", () => {
                 appendScript("huellitas-pet-polish.js", () => {
-                    appendScript("huellitas-profile-mobile.js", () => {
-                        appendScript("huellitas-pet-color.js", () => {
-                            appendScript("huellitas-lost-pets.js", () => {
-                                appendScript("huellitas-admin-secure.js", () => {
-                                    appendScript("huellitas-admin-pro.js", () => appendScript("huellitas-final-polish.js"));
+                    appendScript("huellitas-profile-touch.js", () => {
+                        appendScript("huellitas-profile-mobile.js", () => {
+                            appendScript("huellitas-pet-color.js", () => {
+                                appendScript("huellitas-lost-pets.js", () => {
+                                    appendScript("huellitas-admin-secure.js", () => {
+                                        appendScript("huellitas-admin-pro.js", () => {
+                                            appendScript("huellitas-final-polish.js", () => appendScript("huellitas-ux-guard.js"));
+                                        });
+                                    });
                                 });
                             });
                         });
