2b05b05c472608400f510356af54b15b7dcdc272
carloslira0223-cyber <carloslira0223@gmail.com>
2026-06-30 23:00:33 -0600

    Run Guardianes after legacy game initialization

--- huellitas-guardians-challenge.js
+++ huellitas-guardians-challenge.js
@@ -36,8 +36,11 @@
     let rankingView = "global";
 
     function onReady(callback) {
-        if (document.readyState === "loading") document.addEventListener("DOMContentLoaded", callback, { once: true });
-        else callback();
+        if (document.readyState === "complete") {
+            window.setTimeout(callback, 0);
+            return;
+        }
+        window.addEventListener("load", callback, { once: true });
     }
 
     function loadStyles() {
