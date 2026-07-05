f0afa7f91a091da7cf9e5f5942e3510e5712bf47
carloslira0223-cyber <carloslira0223@gmail.com>
2026-07-05 09:36:12 -0600

    Publica ajustes tactiles finales

--- huellitas.js
+++ huellitas.js
@@ -5,7 +5,7 @@
 (function () {
     "use strict";
 
-    const version = "20260705-tuesday-v21";
+    const version = "20260705-tuesday-v22";
     const scripts = [
         "huellitas-original.js",
         "huellitas-fixes.js",
