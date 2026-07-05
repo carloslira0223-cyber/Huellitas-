dd1fbacccaf8979eb53de7a14d54a83445e62976
carloslira0223-cyber <carloslira0223@gmail.com>
2026-07-05 10:38:13 -0600

    Refresh cache for mobile menu fix

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Proyecto Huellitas - Carlos Alexis Lira Alcala - 2026.
  * Service worker para uso instalable y respaldo sin conexion.
  */
-const CACHE_NAME = "huellitas-pwa-20260705-v12";
+const CACHE_NAME = "huellitas-pwa-20260705-v13";
 const APP_SHELL = [
     "./",
     "./app.html",
