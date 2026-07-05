675f9cdfe7e8f5a1b98da41d82bc7496e9633cd3
carloslira0223-cyber <carloslira0223@gmail.com>
2026-07-05 06:04:07 -0600

    Renueva textos de la estructura principal

--- huellitas.js
+++ huellitas.js
@@ -5,7 +5,7 @@
 (function () {
     "use strict";
 
-    const version = "20260705-structure-v19";
+    const version = "20260705-structure-v20";
     const scripts = [
         "huellitas-original.js",
         "huellitas-fixes.js",
